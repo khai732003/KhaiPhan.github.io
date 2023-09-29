6665efae407911a7c945ec600f4fb3f01fe6f943
Thien An <nguyenphuocthienan2018cm@gmail.com>
2023-09-29 14:49:35 +0700

    Prod

---
+++ Producttt/src/components/ProductPage.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from 'react';
+
+function ProductPage() {
+  const [products, setProducts] = useState([]);
+  const apiUrl = 'https://api.example.com/products';
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setProducts(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []); 
+
+  return (
+    <div className="container">
+      <h1>Product Page</h1>
+      <div className="row">
+        {products.map((product) => (
+          <div className="col-md-4" key={product.id}>
+            <h2>{product.name}</h2>
+            <p>Price: ${product.price}</p>
+            
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default ProductPage;
