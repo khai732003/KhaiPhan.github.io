9e32e21bfb0d99fc15ce68706987e791b7ab2612
CrisBryann <nguyenphuocthienan2018cm@gmail.com>
2023-11-05 21:58:12 +0700

    fix

--- Fekhang/src/content/dashboard/pages/AdminProfile.js
+++ Fekhang/src/content/dashboard/pages/AdminProfile.js
@@ -1,7 +1,105 @@
-import React from 'react'
+import React from 'react';
+import "../styles/adminprofile.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faTwitter, faLinkedin, faCodepen } from '@fortawesome/free-brands-svg-icons';
 
 export default function AdminProfile() {
   return (
-    <div>AdminProfile</div>
-  )
+    <div className="container-adminprofile">
+      <div className="card">
+        <img src="image/img1.jpg" alt="" className="card__image" />
+        <p className="card__name">Lily-Grace Colley</p>
+
+        <div className="grid-container">
+          <div className="grid-child-posts content">156 Post</div>
+          <div className="grid-child-posts content">1012 Like</div>
+        </div>
+        <ul className="social-icons">
+          <li><a href="#"><FontAwesomeIcon icon={faFacebook} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faTwitter} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faCodepen} /></a></li>
+        </ul>
+        <button className="btn draw-border">Follow</button>
+        <button className="btn draw-border">Message</button>
+      </div>
+
+      <div className="card">
+        <img src="image/img2.jpg" alt="" className="card__image" />
+        <p className="card__name">Murray Reeve</p>
+
+        <div className="grid-container">
+          <div className="grid-child-posts content">305 Post</div>
+          <div className="grid-child-posts content">2643 Like</div>
+        </div>
+        <ul className="social-icons">
+          <li><a href="#"><FontAwesomeIcon icon={faFacebook} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faTwitter} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faCodepen} /></a></li>
+        </ul>
+        <button className="btn draw-border">Follow</button>
+        <button className="btn draw-border">Message</button>
+      </div>
+
+      <div className="card">
+        <img src="image/img3.jpg" alt="" className="card__image" />
+        <p className="card__name">Bianca Serrano</p>
+
+        <div className="grid-container">
+          <div className="grid-child-posts content">902 Post</div>
+          <div className="grid-child-posts content">1300 Like</div>
+        </div>
+        <ul className="social-icons">
+          <li><a href="#"><FontAwesomeIcon icon={faFacebook} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faTwitter} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faCodepen} /></a></li>
+        </ul>
+        <button className="btn draw-border">Follow</button>
+        <button className="btn draw-border">Message</button>
+      </div>
+
+      <div className="card">
+        <img src="image/img2.jpg" alt="" className="card__image" />
+        <p className="card__name">Murray Reeve</p>
+
+        <div className="grid-container">
+          <div className="grid-child-posts content">305 Post</div>
+          <div className="grid-child-posts content">2643 Like</div>
+        </div>
+        <ul className="social-icons">
+          <li><a href=""><FontAwesomeIcon icon={faFacebook} /></a></li>
+          <li><a href=""><FontAwesomeIcon icon={faInstagram} /></a></li>
+          <li><a href=""><FontAwesomeIcon icon={faTwitter} /></a></li>
+          <li><a href=""><FontAwesomeIcon icon={faLinkedin} /></a></li>
+          <li><a href=""><FontAwesomeIcon icon={faCodepen} /></a></li>
+        </ul>
+        <button className="btn draw-border">Follow</button>
+        <button className="btn draw-border">Message</button>
+      </div>
+
+      <div className="card">
+        <img src="image/img2.jpg" alt="" className="card__image" />
+        <p className="card__name">Murray Reeve</p>
+
+        <div className="grid-container">
+          <div className="grid-child-posts content">305 Post</div>
+          <div className="grid-child-posts content">2643 Like</div>
+        </div>
+        <ul className="social-icons">
+          <li><a href="#"><FontAwesomeIcon icon={faFacebook} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faInstagram} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faTwitter} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faLinkedin} /></a></li>
+          <li><a href="#"><FontAwesomeIcon icon={faCodepen} /></a></li>
+        </ul>
+        <button className="btn draw-border">Follow</button>
+        <button className="btn draw-border">Message</button>
+      </div>
+    </div>
+  );
 }
--- Fekhang/src/content/dashboard/pages/Revenue.js
+++ Fekhang/src/content/dashboard/pages/Revenue.js
@@ -1,43 +1,98 @@
+// import React, { useState, useEffect } from "react";
+// import "../styles/productmanagement.css";
+// import customAxios from "../../../CustomAxios/customAxios";
+// import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
+
+// const URL = "/doanh-thu";
+
+// const TimeLine = () => {
+//   const [revenueData, setRevenueData] = useState([]);
+
+//   const sampleRevenueData = [
+//     { data: '2023-01-01', revenue: 1000 },
+//     { data: '2023-01-02', revenue: 1200 },
+//     { data: '2023-01-03', revenue: 800 },
+//     { data: '2023-01-04', revenue: 1500 },
+//     { data: '2023-01-05', revenue: 1100 },
+//     { data: '2023-01-06', revenue: 1300 },
+//     // Thêm các bản ghi khác ở đây
+//   ];
+
+//   const getRevenueData = async () => {
+//     try {
+//       const res = await customAxios.get(URL);
+//       setRevenueData(res.data);
+//       console.log(res.data);
+//     } catch (error) {
+//       console.error("Error fetching revenue data:", error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     setRevenueData(sampleRevenueData);
+//   }, []);
+
+//   return (
+//     <div className="user-management-page" style={{ paddingTop: "100px" }}>
+//       <ResponsiveContainer width="100%" height={400}>
+//         <LineChart data={revenueData}>
+//           <CartesianGrid strokeDasharray="3 3" />
+//           <XAxis dataKey="data" />
+//           <YAxis />
+//           <Tooltip />
+//           <Legend />
+//           <Line type="monotone" dataKey="revenue" stroke="#8884d8" name="Doanh thu" />
+//         </LineChart>
+//       </ResponsiveContainer>
+//     </div>
+//   );
+// };
+
+// export default TimeLine;
+
+
+
+
+
+
+
+
 import React, { useState, useEffect } from "react";
 import "../styles/productmanagement.css";
 import customAxios from "../../../CustomAxios/customAxios";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from "recharts";
 
 const URL = "/doanh-thu";
 
 const Revenue = () => {
-  const [products, setProducts] = useState([]);
+  const [revenueData, setRevenueData] = useState([]);
 
-  const getListProducts = async () => {
-    const res = await customAxios.get(`/doanh-thu`);
-    setProducts(res.data);
-    console.log(res.data);
+  const getRevenueData = async () => {
+    try {
+      const res = await customAxios.get(URL);
+      setRevenueData(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.error("Error fetching revenue data:", error);
+    }
   };
 
   useEffect(() => {
-    getListProducts();
+    getRevenueData();
   }, []);
 
   return (
-    <div className="user-management-page" style={{ paddingTop: '100px' }}>
-      <div className="table-staff-container">
-        <table className="user-table" style={{ marginBlock: '100px' }}>
-          <thead>
-            <tr>
-              <th className="user-management-header">{products}</th>
-            </tr>
-          </thead>
-
-          {/* <tbody>
-            {products && products.map((product, index) => (
-              <tr key={product.id}>
-                <td className="user-management-td smaller-text">
-                  {product.data}
-                </td>
-              </tr>
-            ))}
-          </tbody> */}
-        </table>
-      </div>
+    <div className="user-management-page" style={{ paddingTop: "100px" }}>
+      <ResponsiveContainer width="100%" height={400}>
+        <LineChart data={revenueData}>
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="data" />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="revenue" stroke="#8884d8" name="Doanh thu" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
