ec3fadbd71215fdf7a5c25f25dfeaa0840575872
khai732003 <khaithinhvui2003@gmail.com>
2023-11-15 20:29:38 +0700

    fix map

--- Fekhang/src/content/SanPham/Order.js
+++ Fekhang/src/content/SanPham/Order.js
@@ -117,122 +117,132 @@ const Order = () => {
 
   return (
     <div className="order-container">
-  {order && order.orderDetails.length > 0 ? (
-    <Grid container spacing={2}>
-      <Grid item xs={12} md={7} className="order-detail-container">
-        <OrderDetail orderId={order.id} deleteOrderDetail={deleteOrderDetail} />
-      </Grid>
-      <Grid className="voucher-pay" item xs={12} md={5}>
-        <div className="pay-area">
-          <div className="voucher-input">
-            <FormControl>
-              <InputLabel id="voucher-label">Chọn mã voucher</InputLabel>
-              <Select
-                labelId="voucher-label"
-                className="input"
-                value={voucherCode}
-                onChange={(e) => setVoucherCode(e.target.value)}
-              >
-                {voucherData.map((voucher) => (
-                  <MenuItem key={voucher.id} value={voucher.code}>
-                    {voucher.code}
-                  </MenuItem>
-                ))}
-              </Select>
-            </FormControl>
-
-            <Button onClick={applyVoucher} variant="outlined">
-              Áp dụng
-            </Button>
-          </div>
-
-          <Grid
-            className="pay-info"
-            container
-            xs={12}
-            md={12}
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Grid className="info" container xs={12}>
-              <Grid className="local-info" item md={12} xs={12}>
-                <Grid container>
-                  <Grid className="left" item md={6} xs={12}>
-                    Customer Name:
-                  </Grid>
-                  <Grid className="right" item md={6} xs={12}>
-                    {user.fullName}
-                  </Grid>
-                </Grid>
-                <Grid container>
-                  <Grid className="left" item md={6} xs={12}>
-                    Address:
-                  </Grid>
-                  <Grid className="right" item md={6} xs={12}>
-                    {user.address}
-                  </Grid>
-                </Grid>
-                <Grid container>
-                  <Grid className="left" item md={6} xs={12}>
-                    Products price:
-                  </Grid>
-                  <Grid className="right" item md={6} xs={12}>
-                    {formatCurrency(
-                      order.orderDetails.reduce(
-                        (totalPrice, orderDetail) => totalPrice + orderDetail.totalCost,
-                        0
-                      )
-                    )}
-                  </Grid>
-                </Grid>
-                <Grid container>
-                  <Grid className="left" item md={6} xs={12}>
-                    Ship price:
-                  </Grid>
-                  <Grid className="right" item md={6} xs={12}>
-                    {formatCurrency(order.shipPrice)}
-                  </Grid>
-                </Grid>
-                <Grid container>
-                  <Grid className="left" item md={6} xs={12}>
-                    Discount:
-                  </Grid>
-                  <Grid className="right" item md={6} xs={12}>
-                    {discountedPrices.map((price, index) => (
-                      <div key={index}>{formatCurrency(-price)}</div>
+      {order && order.orderDetails.length > 0 ? (
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={7} className="order-detail-container">
+            <OrderDetail orderId={order.id} deleteOrderDetail={deleteOrderDetail} />
+          </Grid>
+          <Grid className="voucher-pay" item xs={12} md={5}>
+            <div className="pay-area">
+              <div className="voucher-input">
+                <FormControl>
+                  <InputLabel id="voucher-label">Chọn mã voucher</InputLabel>
+                  <Select
+                    labelId="voucher-label"
+                    className="input"
+                    value={voucherCode}
+                    onChange={(e) => setVoucherCode(e.target.value)}
+                  >
+                    {voucherData.map((voucher) => (
+                      <MenuItem key={voucher.id} value={voucher.code}>
+                        {voucher.code}
+                      </MenuItem>
                     ))}
-                  </Grid>
-                </Grid>
-                <Grid container>
-                  <Grid className="left" item md={6} xs={12}>
-                    Total:
-                  </Grid>
-                  <Grid className="right" item md={6} xs={12}>
-                    {formatCurrency(order.total_price)}
+                  </Select>
+                </FormControl>
+
+                <Button onClick={applyVoucher} variant="outlined">
+                  Áp dụng
+                </Button>
+              </div>
+
+              <Grid
+                className="pay-info"
+                container
+                xs={12}
+                md={12}
+                justifyContent="center"
+                alignItems="center"
+              >
+                <Grid className="info" container xs={12}>
+                  <Grid className="local-info" item md={12} xs={12}>
+                    <Grid container>
+                      <Grid className="left" item md={6} xs={12}>
+                        Customer Name:
+                      </Grid>
+                      <Grid className="right" item md={6} xs={12}>
+                        {user.fullName}
+                      </Grid>
+                    </Grid>
+                    <Grid container>
+                      <Grid className="left" item md={6} xs={12}>
+                        Address:
+                      </Grid>
+                      <Grid className="right" item md={6} xs={12}>
+                        {user.address}
+                      </Grid>
+                    </Grid>
+                    <Grid container>
+                      <Grid className="left" item md={6} xs={12}>
+                        Products price:
+                      </Grid>
+                      <Grid className="right" item md={6} xs={12}>
+                        {formatCurrency(
+                          order.orderDetails.reduce(
+                            (totalPrice, orderDetail) => totalPrice + orderDetail.totalCost,
+                            0
+                          )
+                        )}
+                      </Grid>
+                    </Grid>
+                    <Grid container>
+                      <Grid className="left" item md={6} xs={12}>
+                        Ship price:
+                      </Grid>
+                      <Grid className="right" item md={6} xs={12}>
+                        {formatCurrency(order.shipPrice)}
+                      </Grid>
+                    </Grid>
+                    <Grid container>
+                      <Grid className="left" item md={6} xs={12}>
+                        Discount:
+                      </Grid>
+                      <Grid className="right" item md={6} xs={12}>
+                        {discountedPrices && discountedPrices.length > 0 && (
+                          <Grid container>
+                            <Grid className="left" item md={6} xs={12}>
+                              Discount:
+                            </Grid>
+                            <Grid className="right" item md={6} xs={12}>
+                              {discountedPrices.map((price, index) => (
+                                <div key={index}>{formatCurrency(-price)}</div>
+                              ))}
+                            </Grid>
+                          </Grid>
+                        )}
+
+                      </Grid>
+                    </Grid>
+                    <Grid container>
+                      <Grid className="left" item md={6} xs={12}>
+                        Total:
+                      </Grid>
+                      <Grid className="right" item md={6} xs={12}>
+                        {formatCurrency(order.total_price)}
+                      </Grid>
+                    </Grid>
                   </Grid>
                 </Grid>
               </Grid>
-            </Grid>
+              <Grid
+                className="confirm"
+                container
+                direction="row"
+                justifyContent="flex-end"
+                alignItems="flex-end"
+              >
+                <ConfirmEmail />
+              </Grid>
+            </div>
           </Grid>
-          <Grid
-            className="confirm"
-            container
-            direction="row"
-            justifyContent="flex-end"
-            alignItems="flex-end"
-          >
-            <ConfirmEmail />
-          </Grid>
-        </div>
-      </Grid>
-    </Grid>
-  ) : (
-    <div className="not-product" >
+        </Grid>
+      ) : (
+        <div className="not-product" >
           <Alert severity="info">NOT FOUND PRODUCT IN HERE</Alert>
-    </div>
+        </div>
 
-  )}
-</div>
+      )}
+    </div>
 
   );
 };
